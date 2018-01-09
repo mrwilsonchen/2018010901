a8584cb2636ae8c10e8b05604b541377e0fe5d5a
C1 <c1@gjun.com>
2018-01-09 11:49:15 +0800

    2

--- app/src/main/java/com/wilson/a2018010901/MainActivity.java
+++ app/src/main/java/com/wilson/a2018010901/MainActivity.java
@@ -2,6 +2,9 @@ package com.wilson.a2018010901;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    on
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("ADD");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getTitle().equals("ADD"))
+        {
+            Toast.makeText(MainActivity.this, "使用者按下 ADD", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
