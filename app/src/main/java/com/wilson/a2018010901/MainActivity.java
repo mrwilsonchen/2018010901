455b36bf15249c214c2df5ed5b928481d4695bc6
C1 <c1@gjun.com>
2018-01-09 11:42:42 +0800

    Initial commit

---
+++ app/src/main/java/com/wilson/a2018010901/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wilson.a2018010901;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    on
+}
